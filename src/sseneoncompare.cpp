5d07447293b8e9de9bf696ad3473eaf3b65341a3
Yining Karl Li <betajippity@gmail.com>
2021-08-01 10:04:53 -0700

    Use steady_clock in timer and fix typo

--- src/sseneoncompare.cpp
+++ src/sseneoncompare.cpp
@@ -10,17 +10,17 @@
 #endif
 
 struct Timer {
-    std::chrono::high_resolution_clock::time_point m_startTime;
+    std::chrono::steady_clock::time_point m_startTime;
 
-    Timer() { m_startTime = std::chrono::high_resolution_clock::now(); }
-    void start() { m_startTime = std::chrono::high_resolution_clock::now(); }
+    Timer() { m_startTime = std::chrono::steady_clock::now(); }
+    void start() { m_startTime = std::chrono::steady_clock::now(); }
 
-    int getElapsedMicoSec() const {
-        auto end_time = std::chrono::high_resolution_clock::now();
-        return (int)std::min(
-            (int)std::chrono::duration_cast<std::chrono::microseconds>(end_time - m_startTime)
+    long getElapsedMicroSec() const {
+        auto end_time = std::chrono::steady_clock::now();
+        return (long)std::min(
+            (long)std::chrono::duration_cast<std::chrono::microseconds>(end_time - m_startTime)
                 .count(),
-            std::numeric_limits<int>::max());
+            std::numeric_limits<long>::max());
     }
 };
 
@@ -324,8 +324,9 @@ int main() {
     BBox bbox1(FVec4(1.5f, 1.5f, 1.5f), FVec4(2.0f, 2.0f, 2.0f));
     BBox bbox2(FVec4(-2.0f, -2.0f, -2.0f), FVec4(2.0f, 2.0f, 2.0f));
     BBox bbox3(FVec4(-1.5f, -1.5f, -1.5f), FVec4(-2.0f, -2.0f, -2.0f));
+    BBox4 bbox4(bbox0, bbox1, bbox2, bbox3);
 
-    auto printResults = [&](const std::string& testName, int elapsedMicroSec, const IVec4& hits,
+    auto printResults = [&](const std::string& testName, long elapsedMicroSec, const IVec4& hits,
                             const FVec4& tMins, const FVec4& tMaxs) {
         std::cout << testName << ": " << elapsedMicroSec << " μs" << std::endl;
         for (size_t i = 0; i < 4; i++) {
@@ -350,24 +351,22 @@ int main() {
     for (int i = 0; i < numTests; i++) {
         rayBBoxIntersect4Scalar(ray, bbox0, bbox1, bbox2, bbox3, hits, tMins, tMaxs);
     }
-    printResults("Scalar", timer.getElapsedMicoSec(), hits, tMins, tMaxs);
+    printResults("Scalar", timer.getElapsedMicroSec(), hits, tMins, tMaxs);
 
     timer.start();
     for (int i = 0; i < numTests; i++) {
         rayBBoxIntersect4ScalarCompact(ray, bbox0, bbox1, bbox2, bbox3, hits, tMins, tMaxs);
     }
-    printResults("Scalar Compact", timer.getElapsedMicoSec(), hits, tMins, tMaxs);
-
-    BBox4 bbox4(bbox0, bbox1, bbox2, bbox3);
+    printResults("Scalar Compact", timer.getElapsedMicroSec(), hits, tMins, tMaxs);
 
     timer.start();
     for (int i = 0; i < numTests; i++) {
         rayBBoxIntersect4SSE(ray, bbox4, hits, tMins, tMaxs);
     }
 #if defined(__x86_64__)
-    printResults("SSE", timer.getElapsedMicoSec(), hits, tMins, tMaxs);
+    printResults("SSE", timer.getElapsedMicroSec(), hits, tMins, tMaxs);
 #elif defined(__aarch64__)
-    printResults("SSE (via sse2neon)", timer.getElapsedMicoSec(), hits, tMins, tMaxs);
+    printResults("SSE (via sse2neon)", timer.getElapsedMicroSec(), hits, tMins, tMaxs);
 #endif
 
 #if defined(__aarch64__)
@@ -375,7 +374,7 @@ int main() {
     for (int i = 0; i < numTests; i++) {
         rayBBoxIntersect4Neon(ray, bbox4, hits, tMins, tMaxs);
     }
-    printResults("Neon", timer.getElapsedMicoSec(), hits, tMins, tMaxs);
+    printResults("Neon", timer.getElapsedMicroSec(), hits, tMins, tMaxs);
 #endif
 
     return 0;
